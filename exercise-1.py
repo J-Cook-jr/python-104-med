7114d3a45e5f3a7b83e5e41cd977ffd8bc3b8b74
Jeff Cook <jecook2692@gmail.com>
2020-04-05 22:51:52 -0400

    adds leet translation

--- exercise-1.py
+++ exercise-1.py
@@ -2,6 +2,11 @@
 
 # Step 1. Define a function that takes one argument.
 def leet(text):
+    
+    #Step 3. Create a function body using the for loop to get the characters that will replace the specified string characters with the leet characters.
+    getchar = lambda c: chars[c] if c in chars else c
+    chars = {"a":"4", "e":"3", "g":"6", "i":"1", "o":"0", "s":"5", "t":"7"}
+    return ''.join(getchar(c) for c in text)
 
 #Step 2. Redefine the function and replace the argument with the string you want to translate.
 print(leet("Leet speak converter."))
