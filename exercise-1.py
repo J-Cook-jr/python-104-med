cac9879e75719b0dba228f85fe7c545358b7668b
Jeff Cook <jecook2692@gmail.com>
2020-04-05 22:35:52 -0400

    adds leet message

---
+++ exercise-1.py
@@ -0,0 +1,8 @@
+# This program creates a function that will translate a string using "Leet Speak" translation.
+
+# Step 1. Define a function that takes one argument.
+def leet(text):
+
+#Step 2. Redefine the function and replace the argument with the string you want to translate.
+print(leet("Leet speak converter."))
+print(leet("Converted to leet speak!"))
